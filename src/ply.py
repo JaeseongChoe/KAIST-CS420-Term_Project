04864fbcf34ae473b3b41fe77cdd8c77f6fed304
mekty2012 <41375658+mekty2012@users.noreply.github.com>
2019-11-06 01:23:18 +0900

    Update ply.py
    It was wrong file

--- src/ply.py
+++ src/ply.py
@@ -1,102 +1,66 @@
-tokens = (
-    'NAME','NUMBER',
-    'PLUS','MINUS','TIMES','DIVIDE','EQUALS',
-    'LPAREN','RPAREN',
-    )
+import dataclasses
+import typing
 
-# Tokens
+@dataclass
+class node:
+  line:int
 
-t_PLUS    = r'\+'
-t_MINUS   = r'-'
-t_TIMES   = r'\*'
-t_DIVIDE  = r'/'
-t_EQUALS  = r'='
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
-t_NAME    = r'[a-zA-Z_][a-zA-Z0-9_]*'
+@dataclass
+class binary_operation(node):
+  op:node
+  lvalue:node
+  rvalue:node
 
-def t_NUMBER(t):
-    r'\d+'
-    try:
-        t.value = int(t.value)
-    except ValueError:
-        print("Integer value too large %d", t.value)
-        t.value = 0
-    return t
+@dataclass
+class array_assignment(node):
+  array:node
+  index:node
+  value:node
 
-# Ignored characters
-t_ignore = " \t"
+@dataclass
+class assignment(node):
+  id:node
+  value:node
 
-def t_newline(t):
-    r'\n+'
-    t.lexer.lineno += t.value.count("\n")
+@dataclass
+class unary_operation(node):
+  operator:node
+  operand:node
 
-def t_error(t):
-    print("Illegal character '%s'" % t.value[0])
-    t.lexer.skip(1)
+@dataclass
+class call(node):
+  fun_name:str
+  params:List[node]
 
-# Build the lexer
-import ply.lex as lex
-lexer = lex.lex()
+@dataclass
+class fun_def(node):
+  fun_name:str
+  params:List[(Type, str)]
+  body:node
 
-# Parsing rules
+@dataclass
+class array_access(node):
+  array:node
+  index:node
 
-precedence = (
-    ('left','PLUS','MINUS'),
-    ('left','TIMES','DIVIDE'),
-    ('right','UMINUS'),
-    )
+@dataclass
+class printf(node):
+  string:node
+  others:List[node]
 
-# dictionary of names
-names = { }
+@dataclass
+class cond_while(node):
+  cond:node
+  body:node
 
-def p_statement_assign(t):
-    'statement : NAME EQUALS expression'
-    names[t[1]] = t[3]
+@dataclass
+class cond_for(node):
+  init:node
+  cond:node
+  body:node
+  after:node
 
-def p_statement_expr(t):
-    'statement : expression'
-    print(t[1])
-
-def p_expression_binop(t):
-    '''expression : expression PLUS expression
-                  | expression MINUS expression
-                  | expression TIMES expression
-                  | expression DIVIDE expression'''
-    if t[2] == '+'  : t[0] = t[1] + t[3]
-    elif t[2] == '-': t[0] = t[1] - t[3]
-    elif t[2] == '*': t[0] = t[1] * t[3]
-    elif t[2] == '/': t[0] = t[1] / t[3]
-
-def p_expression_uminus(t):
-    'expression : MINUS expression %prec UMINUS'
-    t[0] = -t[2]
-
-def p_expression_group(t):
-    'expression : LPAREN expression RPAREN'
-    t[0] = t[2]
-
-def p_expression_number(t):
-    'expression : NUMBER'
-    t[0] = t[1]
-
-def p_expression_name(t):
-    'expression : NAME'
-    try:
-        t[0] = names[t[1]]
-    except LookupError:
-        print("Undefined name '%s'" % t[1])
-        t[0] = 0
-
-def p_error(t):
-    print("Syntax error at '%s'" % t.value)
-
-import ply.yacc as yacc
-parser = yacc.yacc()
-
-while True:
-    try:
-        s = input('calc > ')   # Use raw_input on Python 2
-    except EOFError:
-        break
-    parser.parse(s)
+class if_then_else(node):
+  cond:node
+  then_body:node
+  else_body:node
