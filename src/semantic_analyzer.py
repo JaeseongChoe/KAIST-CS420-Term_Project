a746f9eb89d75cc526bf2405d40d1b0b27d58800
Youngrae Kim <dufo7070@kaist.ac.kr>
2019-11-20 00:38:42 +0900

    Update semantic_analyzer.py : Constructed skeleton code

--- src/semantic_analyzer.py
+++ src/semantic_analyzer.py
@@ -10,3 +10,46 @@ import syntax_analyzer
 # Get AST from syntax analyzer
 # TODO
 
+# Class for semantic analyzer
+class semantic_analyzer:
+	def __init__(self, ast):
+		# Cursor for original AST
+		self.ast = ast
+
+	def binary_same(self, left, right):
+
+
+	def ternary_same(self, left, mid, right):
+
+
+	def type_check(self, cursor):
+		# AST type of current cursor
+		name_node = cursor.get_data[0]
+
+		if name_node == "statement":
+
+		elif name_node == "statement":
+
+		elif name_node == "statement":
+
+		elif name_node == "statement":
+
+		elif name_node == "statement":
+
+		elif name_node == "statement":
+
+		elif name_node == "statement":
+
+		elif name_node == "statement":
+
+		elif name_node == "statement":
+
+		elif name_node == "statement":
+
+		else:
+
+
+	def type_convert(self, node):
+
+
+	def run(self):
