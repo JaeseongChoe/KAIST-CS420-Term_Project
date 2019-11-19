90e4238855911f70a605c20874ede06217f3e0c3
Youngrae Kim <dufo7070@kaist.ac.kr>
2019-11-20 00:02:34 +0900

    Add semantic_analyzer.py

---
+++ src/semantic_analyzer.py
@@ -0,0 +1,12 @@
+# ----------------------------------------------------------------------
+# semantic_analyzer.py
+#
+# A semantic analyzer for ANSI C.
+# ----------------------------------------------------------------------
+
+import sys
+import syntax_analyzer
+
+# Get AST from syntax analyzer
+# TODO
+
