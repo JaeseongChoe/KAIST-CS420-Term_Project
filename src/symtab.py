3f9c6b3cd74a4c841933c6afca12792b604a82c4
Youngrae Kim <dufo7070@kaist.ac.kr>
2019-12-07 06:32:17 +0900

    Update symtab.py: Fix some bugs

--- src/symtab.py
+++ src/symtab.py
@@ -1,43 +1,52 @@
+SYMTAB_ERROR_DUP_DECL = "redeclaration of '%s' with no linkage"
+SYMTAB_ERROR_UNDEF_ID = "'%s' undeclared"
+
+
+class SymTabError(Exception):
+    """Base class for exceptions in SymTab module.
+
+    Attributes:
+        expr -- input expression in which the error occurred
+        mgs -- explanation of the error
+    """
+
+    def __init__(self, expr, msg):
+        self.expr = expr
+        self.msg = msg % expr
+
+
+class DupDeclError(SymTabError):
+    """Exception raised for duplicated declaration error."""
+    pass
+
+
+class UndefIdError(SymTabError):
+    """Exception raised for undefined identifier error."""
+    pass
+
+
 class SymTabEntry:
-    '''
-    This class is entry for symbol table.
-    id denotes name of variable.
-    type denotes type and other properties of variable.
-    assigned denotes whether it is assigned or not. It can be only true when used by interpreter.
-    '''
-    def __init__(self, id: str, type):
+    def __init__(self, id, type, assigned=False):
         self.id = id
         self.type = type
-        self.assigned = False
-
-    '''
-    This function checks whether two entry has same type.
-    '''
-    def type_eq(self, other):
-        raise NotImplementedError
+        self.assigned = assigned
 
 
 class SymTabBlock:
-    '''
-    This is a symbol table block. It works as dict.
-    '''
-    def __init__(self, prev, next = None):
+    def __init__(self, prev=None, nexts=[]):
         self.prev = prev
-        if next is None:
-            self.next = []
+        self.nexts = nexts
         self.table = {}
 
     def insert(self, symbol):
-        if symbol.id in self.table.keys():
-            # TODO : Need to implement (maybe try-except?)
-            pass
+        if symbol.id in self.table:
+            raise DupDeclError(id, SYMTAB_ERROR_DUP_DECL)
         else:
             self.table[symbol.id] = symbol
 
     def remove(self, id):
         if id not in self.table.keys():
-            # TODO : Need to implement (maybe try-except?)
-            pass
+            raise UndefIdError(id, SYMTAB_ERROR_UNDEF_ID)
         else:
             self.table.pop(id)
 
@@ -45,8 +54,7 @@ class SymTabBlock:
         try:
             return self.table[id]
         except KeyError:
-            # TODO : Need to implement
-            pass
+            return None
 
 
 class SymTab:
@@ -55,9 +63,12 @@ class SymTab:
         self.cur = global_table
 
     def insert_block_table(self, block_table):
-        self.cur.next.append(block_table)
+        self.cur.nexts.append(block_table)
         block_table.prev = self.cur
         self.cur = block_table
+
+    def remove_block_table(self):
+        self.cur = self.cur.prev
 
     def insert(self, symbol):
         self.cur.insert(symbol)
@@ -69,13 +80,8 @@ class SymTab:
         table = self.cur
         while table != None:
             entry = table.get(id)
-            if entry != None:
+            if entry == None:
                 table = table.prev
             else:
                 return entry
-        return None
-
-    def rise(self):
-        if self.cur.prev is None:
-            raise RuntimeError("This is root block")
-        self.cur = self.cur.prev
+        raise UndefIdError(id, SYMTAB_ERROR_UNDEF_ID)
