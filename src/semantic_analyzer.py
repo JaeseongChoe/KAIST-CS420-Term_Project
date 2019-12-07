f5a2d390a7cc0d1741172b5a1c17204550aefd4b
Youngrae Kim <dufo7070@kaist.ac.kr>
2019-12-07 21:19:10 +0900

    Update semantic_analyzer.py: Implement type casting

--- src/semantic_analyzer.py
+++ src/semantic_analyzer.py
@@ -49,9 +49,55 @@ class semantic_analyzer:
 		self.symtab = symtab
 		self.lineno = 0
 
-	def binary_compat(self, src, dest, fixed = False):
+	def binary_compat(self, src, dest, line, fixed = False):
 		# Check two types are compatible (i.e. int and float)
-		pass
+		src_type = self.type_check(src)
+		dest_type = self.type_check(dest)
+
+		if not src_type or not dest_type:
+			raise TypeError("Empty attributes", line)
+		elif src_type.pointer != dest_type.pointer:
+			raise TypeError("Wrong dereferencing type", line)
+
+		import copy
+		if src_type.type == dest_type.type:
+			return src_type
+
+		elif src_type.type == 'int' and dest_type.type == 'float':
+			child_node = copy.deepcopy(src)
+			type_spec_node = node.Node('TYPE_SPEC', 'float', line)
+			spec_qual_node = node.Node('SPEC_QUAL', None, line, [type_spec_node])
+			type_name_node = node.Node('TYPE_NAME', None, line, [spec_qual_node])
+			src.type = 'CAST'
+			src.set_value(None)
+			src.children = [type_name_node, child_node]
+			print(f'Type \'int\' has been automatically casted to \'float\' at line {line}')
+			return dest_type
+
+		elif src_type.type == 'float' and dest_type.type == 'int' and fixed:
+			child_node = copy.deepcopy(src)
+			type_spec_node = node.Node('TYPE_SPEC', 'int', line)
+			spec_qual_node = node.Node('SPEC_QUAL', None, line, [type_spec_node])
+			type_name_node = node.Node('TYPE_NAME', None, line, [spec_qual_node])
+			src.type = 'CAST'
+			src.set_value(None)
+			src.children = [type_name_node, child_node]
+			print(f'Type \'float\' has been automatically casted to \'int\' at line {line}')
+			return dest_type
+
+		elif src_type.type == 'float' and dest_type.type == 'int' and not fixed:
+			child_node = copy.deepcopy(dest)
+			type_spec_node = node.Node('TYPE_SPEC', 'float', line)
+			spec_qual_node = node.Node('SPEC_QUAL', None, line, [type_spec_node])
+			type_name_node = node.Node('TYPE_NAME', None, line, [spec_qual_node])
+			dest.type = 'CAST'
+			dest.set_value(None)
+			dest.children = [type_name_node, child_node]
+			print(f'Type \'int\' has been automatically casted to \'float\' at line {line}')
+			return src_type
+
+		else:
+			raise TypeError("Different type", line)
 
 	def binary_same(self, left, right, line):
 		# Check two types are same 
@@ -79,17 +125,21 @@ class semantic_analyzer:
 	def insert_symbol(self, spec_type, cursor, line):
 		# Insert symbol
 		index = 0
+
+		import copy
+		result_type = copy.deepcopy(spec_type)
+
 		if len(cursor.children) == 2:
 			index = 1
-			spec_type.pointer = len(cursor.children[0].children)
+			result_type.pointer = len(cursor.children[0].children)
 
 		if cursor.children[index].type == 'ID':
-			self.symtab.insert(symtab.SymTabEntry(cursor.children[index].get_value(), spec_type))
+			self.symtab.insert(symtab.SymTabEntry(cursor.children[index].get_value(), result_type))
 		
-		elif cursor.children[index].type == 'ARRAY_DECL':
-			spec_type.pointer += 1
-			self.binary_same(self.cursor.children[index].children[1], Type('int'), line)
-			self.symtab.insert(symtab.SymTabEntry(cursor.children[index].children[0].get_value(), spec_type))
+		elif cursor.children[index].type == 'ARR_DECL':
+			result_type.pointer += 1
+			self.binary_compat(cursor.children[index].children[1], Type('int'), line, True)
+			self.symtab.insert(symtab.SymTabEntry(cursor.children[index].children[0].get_value(), result_type))
 
 	def lookup(self, name, line):
 		try:
@@ -103,8 +153,8 @@ class semantic_analyzer:
 			return lookup_type.type
 
 	def lookup_array(self, array_node, id_node, line):
-		self.binary_same(self.id_node, Type('int'), line)
-		name = array_node.children[0].get_value()
+		self.binary_compat(id_node, Type('int'), line, True)
+		name = array_node.get_value().get_value()
 
 		try:
 			lookup_type = self.symtab.get(name)
@@ -113,16 +163,21 @@ class semantic_analyzer:
 
 		if not lookup_type:
 			raise TypeError('Free identifier', line)
-		lookup_type = lookup_type.type
+
+		import copy
+		lookup_type = copy.deepcopy(lookup_type.type)
 
 		if lookup_type.type == 'function' or lookup_type.pointer < 1:
 			raise TypeError('Undefined type', line)
 		else:
-			lookup_type.type.pointer -= 1
-			return lookup_type.type
+			lookup_type.pointer -= 1
+			return lookup_type
 
 	def lookup_function(self, function_node, arg_node, line):
-		name = function_node.children[0].get_value()
+		name = function_node.get_value().get_value()
+		if name == 'printf':
+			return None
+
 		try:
 			lookup_type = self.symtab.get(name)
 		except:
@@ -136,7 +191,7 @@ class semantic_analyzer:
 			raise TypeError('Undefined type', line)
 		else:
 			for i in range(len(arg_node.children)):
-				self.binary_same(self.type_check(arg_node.children[i]), lookup_type.input_type[i], line)
+				self.binary_compat(arg_node.children[i], lookup_type.input_type[i], line, True)
 		
 		return lookup_type.output_type
 
@@ -144,17 +199,17 @@ class semantic_analyzer:
 		# Omit the base cases
 		if not cursor:
 			return None
+		elif isinstance(cursor, Type):
+			return cursor
 
 		# AST type of current cursor
 		cursor_type = cursor.type
 		self.lineno = cursor.lineno
-		print(cursor_type)
 
 		if cursor_type == 'ICONST':
 			return Type('int')
 
 		elif cursor_type == 'FCONST':
-			# TODO : REVERT!
 			return Type('float')
 
 		elif cursor_type == 'CCONST':
@@ -170,7 +225,10 @@ class semantic_analyzer:
 			return self.lookup_array(cursor.children[0], cursor.children[1], self.lineno)
 
 		elif cursor_type == 'FUNCTION':
-			return self.lookup_function(cursor.children[0], cursor.children[1], self.lineno)
+			return self.lookup_function(cursor.children[0], cursor.children[1].get_value(), self.lineno)
+
+		elif cursor_type == 'INDEX':
+			return self.type_check(cursor.get_value())
 
 		elif cursor_type == 'POSTINC':
 			return self.type_check(cursor.get_value())
@@ -200,27 +258,27 @@ class semantic_analyzer:
 			return self.type_check(cursor.children[0].children[0])
 
 		elif cursor_type == 'MUL_EXPR' or cursor_type == 'ADD_EXPR':
-			return self.binary_same(self.type_check(cursor.children[0]), self.type_check(cursor.children[1]), self.lineno)
+			return self.binary_compat(cursor.children[0], cursor.children[1], self.lineno)
 
 		elif cursor_type == 'SHIFT_EXPR':
 			self.type_check(cursor.children[1])
 			return self.type_check(cursor.children[0])
 
 		elif cursor_type == 'REL_EXPR' or cursor_type == 'EQ_EXPR':
-			self.binary_same(self.type_check(cursor.children[0]), self.type_check(cursor.children[1]), self.lineno)
+			self.binary_compat(cursor.children[0], cursor.children[1], self.lineno)
 			return Type('int')
 
 		elif cursor_type == 'AND_EXPR' or cursor_type == 'XOR_EXPR' or cursor_type == 'OR_EXPR' or cursor_type == 'L_AND_EXPR' or cursor_type == 'L_OR_EXPR':
-			self.binary_same(self.type_check(cursor.children[1]), Type('int'), self.lineno)
-			self.binary_same(self.type_check(cursor.children[0]), Type('int'), self.lineno)
+			self.binary_compat(cursor.children[1], Type('int'), self.lineno, True)
+			self.binary_compat(cursor.children[0], Type('int'), self.lineno, True)
 			return Type('int')
 
 		elif cursor_type == 'TERNARY':
-			self.binary_same(self.type_check(cursor.children[0]), Type('int'), self.lineno)
-			return self.binary_same(self.type_check(cursor.children[1]), self.type_check(cursor.children[2]), self.lineno)
+			self.binary_compat(cursor.children[0], Type('int'), self.lineno, True)
+			return self.binary_compat(cursor.children[1], cursor.children[2], self.lineno)
 
 		elif cursor_type == 'ASSIGN_EXPR':
-			self.binary_same(self.type_check(cursor.children[0]), self.type_check(cursor.children[1]), self.lineno)
+			self.binary_compat(cursor.children[1], cursor.children[0], self.lineno, True)
 			return None
 
 		elif cursor_type == 'EXPR':
@@ -231,6 +289,9 @@ class semantic_analyzer:
 			else:
 				return post_type
 
+		elif cursor_type == 'CONST_EXP':
+			return  self.type_check(cursor.children[0])
+
 		elif cursor_type == 'DECLARATION':
 			if len(cursor.children) == 2:
 				spec_type = self.type_check(cursor.children[0])
@@ -239,7 +300,7 @@ class semantic_analyzer:
 				if spec_type and init_type:
 					for init_child in init_type.children:
 						if init_child.type:
-							self.binary_same(init_child, spec_type, self.lineno)
+							self.binary_compat(cursor.children[1], cursor.children[0], self.lineno, True)
 						self.insert_symbol(spec_type, init_child.children[0], self.lineno)
 
 			return None
@@ -278,9 +339,12 @@ class semantic_analyzer:
 				if cursor.children[0].type == 'ID':
 					child_type = Type('id', cursor.children[0].get_value())
 				
-				elif cursor.children[0].type == 'ARRAY_DECL':
+				elif cursor.children[0].type == 'ARR_DECL':
 					child_type = Type('array', cursor.children[0].children[0].get_value())
-					self.binary_same(self.cursor.children[0].children[1], Type('int'), self.lineno)
+					self.binary_compat(cursor.children[0].children[1], Type('int'), self.lineno, True)
+
+				else:
+					raise TypeError('Different type', self.lineno)
 				
 				return child_type
 			
@@ -289,11 +353,14 @@ class semantic_analyzer:
 				if cursor.children[1].type == 'ID':
 					child_type = Type('id', cursor.children[1].get_value())
 				
-				elif cursor.children[1].type == 'ARRAY_DECL':
+				elif cursor.children[1].type == 'ARR_DECL':
 					child_type = Type('array', cursor.children[1].children[0].get_value())
-					self.binary_same(self.cursor.children[1].children[1], Type('int'), self.lineno)
+					self.binary_compat(cursor.children[1].children[1], Type('int'), self.lineno, True)
+
+				if not child_type:
+					raise TypeError('Different type', self.lineno)
 				
-				if isinstance(child_type, node.Node):
+				if isinstance(child_type, Type):
 					child_type.pointer = len(cursor.children[0].children)
 
 				return child_type
@@ -323,6 +390,8 @@ class semantic_analyzer:
 			if len(cursor.children) == 2:
 				self.type_check(cursor.children[0])
 				result_type = self.type_check(cursor.children[1])
+			elif len(cursor.children) == 1:
+				result_type = self.type_check(cursor.children[0])
 
 			self.symtab.remove_block_table()
 			return result_type
@@ -345,7 +414,7 @@ class semantic_analyzer:
 			return Type('stmt_list', None, children = child_type_list)
 
 		elif cursor_type == 'IF' or cursor_type == 'WHILE' or cursor_type == 'DO_WHILE':
-			self.binary_same(self.type_check(cursor.get_value()), Type('int'), self.lineno)
+			self.binary_compat(cursor.children[0], Type('int'), self.lineno, True)
 			child_type_list = []
 			for child in cursor.children:
 				child_type = self.type_check(child)
@@ -356,8 +425,8 @@ class semantic_analyzer:
 			return Type('stmt_list', None, children = child_type_list)
 
 		elif cursor_type == 'SWITCH':
-			switch_type = self.type_check(cursor.get_value())
-			case_type = self.type_check(cursor.children[0])
+			switch_type = self.type_check(cursor.children[0])
+			case_type = self.type_check(cursor.children[1])
 
 			def case_type_check(cur_type, result):
 				if cur_type.type == 'case':
@@ -377,9 +446,9 @@ class semantic_analyzer:
 		elif cursor_type == 'FOR':
 			for i in range(len(cursor.children)):
 				if cursor.children[i].type != 'EXPR_OPT':
-					child_type = self.type_check(cursor.children[i])
+					self.type_check(cursor.children[i])
 					if i == 1:
-						self.binary_same(child_type, Type('int'), self.lineno)
+						self.binary_compat(cursor.children[i], Type('int'), self.lineno, True)
 			return self.type_check(cursor.get_value())
 
 		elif cursor_type == 'RETURN':
@@ -388,8 +457,12 @@ class semantic_analyzer:
 			return self.type_check(cursor.children[0])
 
 		elif cursor_type == 'TRSL_UNIT':
+			self.symtab.insert_block_table(symtab.SymTabBlock(None))
+
 			for child in cursor.children:
 				self.type_check(child)
+
+			self.symtab.remove_block_table()
 			return None
 
 		elif cursor_type == 'FUNC_DEF':
@@ -441,9 +514,6 @@ class semantic_analyzer:
 		self.type_check(self.cursor)
 		return self.cursor
 
-	def type_convert(self, node):
-		pass
-
 # Open the source code
 f = open("../test/mandatory_example.c", 'r')
 s = f.read()
@@ -454,7 +524,7 @@ lexer = lexical_analyzer.lexical_analyzer
 
 # Get AST from syntax analyzer
 parser = syntax_analyzer.parser
-ast = parser.parse('float f() {int a; float b; a = 10; b = (float) a; return b;}', lexer = lexer)
+ast = parser.parse(s, lexer = lexer)
 
 # Initialize semantic analyer
 semantic_analyzer = semantic_analyzer(ast, symtab.SymTab())
@@ -471,5 +541,6 @@ def print_ast(cursor, depth):
 	for child in cursor.children:
 		print_ast(child, depth + 1)
 
-if checked_ast:
-	print_ast(checked_ast, 0)
+# Check
+# if checked_ast:
+# 	print_ast(checked_ast, 0)
